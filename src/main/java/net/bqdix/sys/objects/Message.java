6e5f253c5e762b709080cf8426bc8e19e2fedb3e
xbqdix <badikplay@gmail.com>
2025-09-29 16:11:05 +0500

    Adding a database to a web application.

---
+++ src/main/java/net/bqdix/sys/objects/Message.java
@@ -0,0 +1,54 @@
+package net.bqdix.sys.objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Integer id;
+    private String text;
+    private String tag;
+
+    public Message() {
+    }
+
+    public Message(Integer id) {
+        this.id = id;
+    }
+
+    public Message(String tag, String text) {
+        this.tag = tag;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
+
